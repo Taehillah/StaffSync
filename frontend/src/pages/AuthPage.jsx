b286462420e789ffc4bd2bc4d30cf12dd8c4da21
Taehillah <ishmaelmafole@gmail.com>
2025-04-19 14:47:49 +0200

    Interface Complete

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginForm from '../components/auth/LoginForm';
 import RegisterForm from '../components/auth/RegisterForm';
+import ForgotPassword from '../components/auth/ForgotPassword';
 
 const AuthPage = () => {
   return (
     <div className="auth-page">
       <Routes>
         <Route path="login" element={<LoginForm />} />
+       
         <Route path="register" element={<RegisterForm />} />
         <Route path="*" element={<LoginForm />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
